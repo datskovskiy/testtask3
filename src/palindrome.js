e63cd183b7d2974387605f3798fa9b6d3af091ca
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-04-02 16:41:18 +0300

    Implement code

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,6 +1,15 @@
 const detectPalindrome = (str) => {
-    // Your implementation
-    // Read README.md file, if you not understand what to do
+    if (typeof str != 'string')
+        return 'Passed argument is not a string';
+
+    if (str.length == 0)
+        return 'String is empty';
+
+    let reverseStr = str.split('').reverse().join('');
+
+    return (reverseStr.toLowerCase() == str.toLowerCase()) 
+        ? 'This string is palindrome!' 
+        : 'This string is not a palindrome!';
 };
 
 module.exports = detectPalindrome;
